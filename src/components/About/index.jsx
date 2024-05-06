41dd844d1c968baf40b7a6c82abb8adc04467b4f
vidhya-metacell <vidhya@metacell.us>
2024-05-06 18:57:13 +0200

    #ilex-6 Import data from json files

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,11 +1,10 @@
-import { Box, Container, Grid, IconButton, List, ListItem, ListItemButton, ListItemText, Typography } from "@mui/material";
+import { Box, Container, Grid, List, ListItem, ListItemButton, ListItemText, Typography } from "@mui/material";
 import { vars } from "../../theme/variables";
-import { ForwardIcon, NavIcon } from "../../Icons";
-// import PropTypes from 'prop-types';
+import { ForwardIcon } from "../../Icons";
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import React from "react";
-
+import TabsData from "../../static/AboutTabs.json";
 const { gray50, gray700, gray500 } = vars;
 
 const style = {
@@ -109,135 +108,52 @@ const About = () => {
                 <Box mb="10rem">
                     <Grid container spacing={0}>
                         <Grid item xs={2}>
-                            <Tabs
+                        <Tabs
                                 orientation="vertical"
                                 variant="scrollable"
                                 value={value}
                                 onChange={handleChange}
-                            >
-                                <Tab label="For Researchers" {...a11yProps(0)} />
-                                <Tab label="For Curators" {...a11yProps(1)} />
-                                <Tab label="For Developers" {...a11yProps(2)} />
-                            </Tabs>
+                        >
+                            {
+                                TabsData.tabs.map((tab, index) => (
+                                    <Tab label={tab?.heading} {...a11yProps(index)} key={tab?.heading} />
+                                ))
+                            }                          
+                        </Tabs>      
                         </Grid>
                         <Grid item xs={10}>
-                            <TabPanel value={value} index={0}>
-                                <Grid container spacing={4}>
-                                    <Grid item xs={9}>
-                                        <Typography sx={{ ...style.headingSecondary, mb: '1.5rem' }}>
-                                            Short What InterLex Offers - Getting Started
-                                        </Typography>
+                            {
+                                TabsData?.tabs?.map((tab, index) => (
+                                    <TabPanel value={value} index={index} key={tab?.heading+index}>
+                                        <Grid container spacing={4}>
+                                            <Grid item xs={9}>
+                                                <Typography sx={{ ...style.headingSecondary, mb: '1.5rem' }}>
+                                                    {tab?.content?.heading}
+                                                </Typography>
 
-                                        <Typography sx={style.descriptionSecondary}>
-                                            The InterLex project - a core component of SciCrunch and supported by projects such as the Neuroscience Information Framework project (NIF), the NIDDK Information Network (dkNET), and the Open Data Commons for Spinal Cord Injury - is a dynamic lexicon of biomedical terms. Unlike an encyclopedia, a lexicon provides the meaning of a term, and not all there is to know about it. InterLex is being constructed to help improve the way that biomedical scientists communicate about their data, so that information systems like NIF and dkNET can find data more easily and provide more powerful means of integrating that data across distributed resources.
-                                        </Typography>
-                                    </Grid>
+                                                <Typography sx={style.descriptionSecondary}>
+                                                    {tab?.content?.text}
+                                                </Typography>
+                                            </Grid>
 
-                                    <Grid item xs={3}>
-                                        <Typography sx={{ ...style.headingSecondary, mb: '1.5rem' }}>Quick links</Typography>
+                                            <Grid item xs={3}>
+                                                <Typography sx={{ ...style.headingSecondary, mb: '1.5rem' }}>Quick links</Typography>
 
-                                        <List sx={style.list}>
-                                            <ListItem>
-                                                <ListItemButton component="a" href="#simple-list">
-                                                    <ListItemText primary={<><Typography>Personal dashboard</Typography><ForwardIcon /></>} />
-                                                </ListItemButton>
-                                            </ListItem>
-
-                                            <ListItem>
-                                                <ListItemButton component="a" href="#simple-list">
-                                                    <ListItemText primary={<><Typography>Organization dashboard</Typography><ForwardIcon /></>} />
-                                                </ListItemButton>
-                                            </ListItem>
-
-                                            <ListItem>
-                                                <ListItemButton component="a" href="#simple-list">
-                                                    <ListItemText primary={<><Typography>Tutorials</Typography><ForwardIcon /></>} />
-                                                </ListItemButton>
-                                            </ListItem>
-
-                                            <ListItem>
-                                                <ListItemButton component="a" href="#simple-list">
-                                                    <ListItemText primary={<><Typography>Use cases documentation</Typography><ForwardIcon /></>} />
-                                                </ListItemButton>
-                                            </ListItem>
-                                        </List>
-                                    </Grid>
-                                </Grid>
-                            </TabPanel>
-                            <TabPanel value={value} index={1}>
-                                <Grid container spacing={4}>
-                                    <Grid item xs={9}>
-                                        <Typography sx={{ ...style.headingSecondary, mb: '1.5rem' }}>
-                                            Short What InterLex Offers - Getting Started
-                                        </Typography>
-
-                                        <Typography sx={style.descriptionSecondary}>
-                                            One of the big roadblocks to data integration in the biomedical sciences is the inconsistent use of terminology in databases and other resources such as the literature. When we use the same terms to mean different things, we cannot easily ask questions that span across multiple resources. For example, if three databases have information about what genes are expressed in cortex, but they all use different definitions of cerebral cortex, then it is hard to compare them. InterLex allows for the association of data values (i.e. the value of a field or text within a field) to terminologies enabling the crowdsourcing of data-terminology mappings.
-                                        </Typography>
-                                    </Grid>
-
-                                    <Grid item xs={3}>
-                                        <Typography sx={{ ...style.headingSecondary, mb: '1.5rem' }}>Quick links</Typography>
-
-                                        <List sx={style.list}>
-                                            <ListItem>
-                                                <ListItemButton component="a" href="#simple-list">
-                                                    <ListItemText primary={<><Typography>Personal dashboard</Typography><ForwardIcon /></>} />
-                                                </ListItemButton>
-                                            </ListItem>
-
-                                            <ListItem>
-                                                <ListItemButton component="a" href="#simple-list">
-                                                    <ListItemText primary={<><Typography>Organization dashboard</Typography><ForwardIcon /></>} />
-                                                </ListItemButton>
-                                            </ListItem>
-
-                                            <ListItem>
-                                                <ListItemButton component="a" href="#simple-list">
-                                                    <ListItemText primary={<><Typography>Organization terms </Typography><ForwardIcon /></>} />
-                                                </ListItemButton>
-                                            </ListItem>
-                                        </List>
-                                    </Grid>
-                                </Grid>
-                            </TabPanel>
-                            <TabPanel value={value} index={2}>
-                                <Grid container spacing={4}>
-                                    <Grid item xs={9}>
-                                        <Typography sx={{ ...style.headingSecondary, mb: '1.5rem' }}>
-                                            Short What InterLex Offers - Getting Started
-                                        </Typography>
-
-                                        <Typography sx={style.descriptionSecondary}>
-                                            InterLex was built on the foundation of NeuroLex (see Larson and Martone 2013 Neurolex: An online framework for neuroscience knowledge. Frontiers in Neuroinformatics, 7:18) and contains all of the existing NeuroLex terms. The initial entries in NeuroLex were built from the NIFSTD ontologies. NIFSTD currently has about 60,000 concepts (includes both classes and synonyms) that span gross anatomy, cells, subcellular structures, diseases, functions and techniques. InterLex models terms using primitives of the Web Ontology Language (OWL) and can export directly to a variety of standard ontology formats.
-                                        </Typography>
-                                    </Grid>
-
-                                    <Grid item xs={3}>
-                                        <Typography sx={{ ...style.headingSecondary, mb: '1.5rem' }}>Quick links</Typography>
-
-                                        <List sx={style.list}>
-                                            <ListItem>
-                                                <ListItemButton component="a" href="#simple-list">
-                                                    <ListItemText primary={<><Typography>Personal dashboard</Typography><ForwardIcon /></>} />
-                                                </ListItemButton>
-                                            </ListItem>
-
-                                            <ListItem>
-                                                <ListItemButton component="a" href="#simple-list">
-                                                    <ListItemText primary={<><Typography>Organization dashboard</Typography><ForwardIcon /></>} />
-                                                </ListItemButton>
-                                            </ListItem>
-
-                                            <ListItem>
-                                                <ListItemButton component="a" href="#simple-list">
-                                                    <ListItemText primary={<><Typography>API documentation</Typography><ForwardIcon /></>} />
-                                                </ListItemButton>
-                                            </ListItem>
-                                        </List>
-                                    </Grid>
-                                </Grid>
-                            </TabPanel>
+                                                <List sx={style.list}>
+                                                    {   tab?.content?.links?.map((link, index) => (
+                                                            <ListItem key={link?.label+index}>
+                                                                <ListItemButton component="a" href={link?.url}>
+                                                                    <ListItemText primary={<><Typography>{link?.label}</Typography><ForwardIcon /></>} />
+                                                                </ListItemButton>
+                                                            </ListItem>
+                                                        ))
+                                                    }
+                                                </List>
+                                            </Grid>
+                                        </Grid>
+                                    </TabPanel>
+                                ))
+                            }
                         </Grid>
                     </Grid>
                 </Box>
--- src/components/Partners/index.jsx
+++ src/components/Partners/index.jsx
@@ -6,6 +6,7 @@ import FEATHERDEV from "../../Icons/svg/featherdev.svg";
 import SPHERULE from "../../Icons/svg/spherule.svg";
 import GLOBALBANK from "../../Icons/svg/globalbank.svg";
 import NIETZSCHE from "../../Icons/svg/nietzsche.svg";
+import PartnersData from "../../static/Partners.json";
 
 const { gray50, gray600 } = vars;
 
@@ -25,18 +26,29 @@ const style = {
     }
 }
 
+const imageMap = {
+    BOLTSHIFT,
+    LIGHTBOX,
+    FEATHERDEV,
+    SPHERULE,
+    GLOBALBANK,
+    NIETZSCHE,
+};
 const Partners = () => {
     return (
         <Box sx={style.root}>
             <Container maxWidth="xl">
                 <Typography sx={style.heading}>Join 4,000+ companies already growing</Typography>
                 <Box display='flex' alignItems='center' justifyContent='center' gap={3}>
-                    <img src={BOLTSHIFT} alt="" />
-                    <img src={LIGHTBOX} alt="" />
-                    <img src={FEATHERDEV} alt="" />
-                    <img src={SPHERULE} alt="" />
-                    <img src={GLOBALBANK} alt="" />
-                    <img src={NIETZSCHE} alt="" />
+                {PartnersData?.images?.map((image, index) => {
+                    const imageSrc = imageMap[image.name];
+                    if (imageSrc) {
+                    return (
+                        <img key={index} src={imageSrc} alt={image.alt} />
+                    );
+                    }
+                    return null;
+                })}
                 </Box>
             </Container>
         </Box>
