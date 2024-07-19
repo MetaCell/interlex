f598bc1d8934107dfb3bee4151a5fe02e7dc7225
jrmartin <yeshuarmartinez@gmail.com>
2024-07-19 04:58:02 -0700

    #ILEX-26 - Connect Organizations page to endpoint data

--- mock/data/mockOrganizations.ts
+++ mock/data/mockOrganizations.ts
@@ -2,8 +2,8 @@ export const mockOrganization = () => { return {
     name: "SPARC Anatomical Working Group",
     description : "The Stimulating Peripheral Activity to Relieve Conditions (SPARC) effort is the result of National Institutes of Health’s (NIH) drive to map out the neural circuitry responsible for visceral control in higher vertebrates. The SPARC Anatomy Working Group (SAWG) is responsible for the integrity of anatomical knowledge in SPARC. In particular, it provides the relevant guidance and expertise about: Defining and naming anatomical terms, Maintaining ontologies of anatomical knowledge, Compiling and curating computable knowledge about multiscale routes pathways",
     id: "ilx_0101431",
-    url: "sparc.science.com",
-    icon : "/organization_icon_1"
+    url: "https://dknet.org",
+    icon : "//logos/NIDDK.svg"
   }
   }
 
@@ -12,14 +12,14 @@ export const mockOrganization = () => { return {
       name: "SPARC Anatomical Working Group 1",
       description : "The Stimulating Peripheral Activity to Relieve Conditions (SPARC) effort is the result of National Institutes of Health’s (NIH) drive to map out the neural circuitry responsible for visceral control in higher vertebrates. The SPARC Anatomy Working Group (SAWG) is responsible for the integrity of anatomical knowledge in SPARC. In particular, it provides the relevant guidance and expertise about: Defining and naming anatomical terms, Maintaining ontologies of anatomical knowledge, Compiling and curating computable knowledge about multiscale routes pathways",
       id: "org_1",
-      url: "kcore.science.com",
-      icon : "/organization_icon_1"
+      url: "https://drugdesigndata.org",
+      icon : "/logos/drugDesign.svg"
     },
     {
       name: "SPARC Anatomical Working Group 2",
       description : "The Stimulating Peripheral Activity to Relieve Conditions (SPARC) effort is the result of National Institutes of Health’s (NIH) drive to map out the neural circuitry responsible for visceral control in higher vertebrates. The SPARC Anatomy Working Group (SAWG) is responsible for the integrity of anatomical knowledge in SPARC. In particular, it provides the relevant guidance and expertise about: Defining and naming anatomical terms, Maintaining ontologies of anatomical knowledge, Compiling and curating computable knowledge about multiscale routes pathways",
       id: "org_2",
-      url: "sparc.science.com",
-      icon : "/organization_icon_2"
+      url: "https://dknet.org",
+      icon : "/logos/NIDDK.svg"
     }
   ]
