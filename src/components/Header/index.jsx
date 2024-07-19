ebb24aa2cade712b517d198743ec7183b7e658e6
Aiga115 <aiguljhyldyzbekkyzy@gmail.com>
2024-07-19 16:41:18 +0200

    ILEX-63 add term editor option to nav header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -10,9 +10,11 @@ import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import Search from './Search';
+import EditBulkTermsDialog from '../Dashboard/EditBulkTermsDialog';
 import { useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import { GlobalDataContext } from "./../../contexts/DataContext";
+import ModeEditOutlineOutlinedIcon from "@mui/icons-material/ModeEditOutlineOutlined";
 
 const { gray200, white, gray100, gray600 } = vars;
 
@@ -120,6 +122,8 @@ const UserNavMenu = [
 const Header = ({ isLoggedIn = true }) => {
     const [anchorEl, setAnchorEl] = React.useState(null);
     const [anchorElUser, setAnchorElUser] = React.useState(null);
+    const [openEditBulkTerms, setOpenEditBulkTerms] = React.useState(false);
+    const [activeStep, setActiveStep] = React.useState(0);
     const { user, setUserData } = useContext(GlobalDataContext);
 
     const handleSetUserData = (user, organization) => {
@@ -147,6 +151,15 @@ const Header = ({ isLoggedIn = true }) => {
         navigate('curie-editor')
     };
 
+    const handleCloseEditBulkTerms = () => {
+        setOpenEditBulkTerms(false)
+        setActiveStep(0)
+    }
+
+    const handleOpenEditBulkTerms = () => {
+        setOpenEditBulkTerms(true)
+    }
+
     const open = Boolean(anchorEl);
     const id = open ? 'simple-popover' : undefined;
 
@@ -167,11 +180,11 @@ const Header = ({ isLoggedIn = true }) => {
         navigate(menu.href)
     }
 
-    
+
     const handleLogoClick = () => {
         navigate('/')
     }
-  
+
     React.useEffect(() => {
         // TODO : Move to login page and remove this proof of concept call
         handleSetUserData("Interlex User", "Interlex");
@@ -197,152 +210,30 @@ const Header = ({ isLoggedIn = true }) => {
     }, [user])
 
     return (
-        <Box sx={styles.root}>
-            <Box width={isLoggedIn ? '15.5625rem' : '23.875rem'} display='flex' gap='1.25rem'>
-                <Button sx={{ p: '0.625rem 0.5625rem', minWidth: '0.0625rem' }} onClick={handleClick} aria-describedby={id} variant='outlined'>
-                    <NavIcon />
-                </Button>
-
-                <Popover
-                    sx={styles.popover}
-                    id={id}
-                    open={open}
-                    anchorEl={anchorEl}
-                    onClose={handleClose}
-                    anchorOrigin={{
-                        vertical: 'bottom',
-                        horizontal: 'left',
-                    }}
-                    transformOrigin={{
-                        vertical: 'top',
-                        horizontal: 'left',
-                    }}
-                >
-                    <List>
-                        {NavMenu.map((menu, index) => (
-                            <ListItem key={index} disablePadding>
-                                <ListItemButton onClick={(e) => handleMenuClick(e, menu)}>
-                                    <ListItemIcon>
-                                        {menu.icon}
-                                    </ListItemIcon>
-                                    <ListItemText primary={menu.label} />
-                                </ListItemButton>
-                            </ListItem>
-                        ))}
-                        <Divider sx={{ mt: 0.5, mb: 0.5, color: gray200 }} />
-                        <ListItem disablePadding onClick={handleClickCurieEditor}>
-                            <ListItemButton>
-                                <ListItemIcon>
-                                    <SortIcon />
-                                </ListItemIcon>
-                                <ListItemText primary={'Curie Editor'} />
-                            </ListItemButton>
-                        </ListItem>
-                    </List>
-                </Popover>
-                <a href="/" style={{cursor: 'pointer'}}>
-                    <img src={Logo} alt="Interlex"/>
-                </a>
-            </Box>
-
-            <Box sx={{ width: '35%', maxWidth: '45.5rem' }}>
-                <Search />
-            </Box>
-
-            {!isLoggedIn ? (
-                <Box display='flex' gap='1.25rem'>
-                    <Box display='flex' gap='0.25rem'>
-                        <Button>Register</Button>
-                        <Button variant="outlined">Log in</Button>
-                    </Box>
-                    <Divider sx={styles.divider} />
-                    <Button variant="contained">
-                        <AddIcon />
-                        Add a new term
+        <>
+            <Box sx={styles.root}>
+                <Box width={isLoggedIn ? '15.5625rem' : '23.875rem'} display='flex' gap='1.25rem'>
+                    <Button sx={{ p: '0.625rem 0.5625rem', minWidth: '0.0625rem' }} onClick={handleClick} aria-describedby={id} variant='outlined'>
+                        <NavIcon />
                     </Button>
-                </Box>
-            ) : (
-                <Box display='flex' gap='1.25rem'>
-                    <Button variant="contained">
-                        <AddIcon />
-                        Add a new term
-                    </Button>
-                    <Divider sx={styles.divider} />
-                    <IconButton sx={{
-                        p: 0,
-                        borderRadius: '50%',
-                        '&:focus': {
-                            boxShadow: '0 0 0 0.25rem rgba(152, 162, 179, 0.14)'
-                        }
-                    }}
-                        onClick={handleUserClick} aria-describedby={idUser}
-                    >
-                        <Avatar sx={{ border: '0.0469rem solid rgba(0,0,0,0.08)', width: '2.5rem', height: '2.5rem' }} src="https://mui.com/static/images/avatar/1.jpg" />
-                    </IconButton>
+
                     <Popover
-                        sx={{
-                            ...styles.popover,
-                            '& .MuiPopover-paper': {
-                                padding: '0',
-                                marginTop: '0.5rem',
-                            },
-
-                            '& .MuiListItem-root + .MuiListItem-root': {
-                                borderTop: `0.0625rem solid #D3D9D8`,
-                            },
-                            '& .MuiListItem-root': {
-                                padding: '0.3125rem 0.375rem'
-                            }
-                        }}
-                        id={idUser}
-                        open={openUser}
-                        anchorEl={anchorElUser}
-                        onClose={handleUserClose}
+                        sx={styles.popover}
+                        id={id}
+                        open={open}
+                        anchorEl={anchorEl}
+                        onClose={handleClose}
                         anchorOrigin={{
                             vertical: 'bottom',
-                            horizontal: 'right',
+                            horizontal: 'left',
                         }}
                         transformOrigin={{
                             vertical: 'top',
-                            horizontal: 'right',
+                            horizontal: 'left',
                         }}
                     >
                         <List>
-                            <ListItem sx={{
-                                p: '0.75rem 1rem !important',
-
-                                '& .MuiListItemText-root .MuiListItemText-primary': {
-                                    fontSize: '0.875rem',
-                                    fontWeight: 600,
-                                    lineHeight: '142.857%',
-                                    color: '#3B403F'
-                                },
-
-                                '& .MuiListItemText-root .MuiListItemText-secondary': {
-                                    fontSize: '0.875rem',
-                                    fontWeight: 400,
-                                    lineHeight: '142.857%',
-                                    color: '#4D4F4F'
-                                },
-                            }}>
-                                <ListItemAvatar>
-                                    <Badge
-                                        variant="dot"
-                                        color="success"
-                                        anchorOrigin={{
-                                            vertical: 'bottom',
-                                            horizontal: 'right',
-                                        }}
-                                    >
-                                        <Avatar sx={{ border: '0.0469rem solid rgba(0,0,0,0.08)', width: '2.5rem', height: '2.5rem' }} src="https://mui.com/static/images/avatar/1.jpg" />
-                                    </Badge>
-                                </ListItemAvatar>
-                                <ListItemText
-                                    primary="Olivia Rhye"
-                                    secondary="olivia@untitledui.com"
-                                />
-                            </ListItem>
-                            {UserNavMenu.map((menu, index) => (
+                            {NavMenu.map((menu, index) => (
                                 <ListItem key={index} disablePadding>
                                     <ListItemButton onClick={(e) => handleMenuClick(e, menu)}>
                                         <ListItemIcon>
@@ -352,11 +243,144 @@ const Header = ({ isLoggedIn = true }) => {
                                     </ListItemButton>
                                 </ListItem>
                             ))}
+                            <Divider sx={{ mt: 0.5, mb: 0.5, color: gray200 }} />
+                            <ListItem disablePadding onClick={handleClickCurieEditor}>
+                                <ListItemButton>
+                                    <ListItemIcon>
+                                        <SortIcon />
+                                    </ListItemIcon>
+                                    <ListItemText primary={'Curie Editor'} />
+                                </ListItemButton>
+                            </ListItem>
+                            <ListItem disablePadding onClick={handleOpenEditBulkTerms}>
+                                <ListItemButton>
+                                    <ListItemIcon>
+                                        <ModeEditOutlineOutlinedIcon fontSize='small' />
+                                    </ListItemIcon>
+                                    <ListItemText primary={'Terms editor'} />
+                                </ListItemButton>
+                            </ListItem>
                         </List>
                     </Popover>
+                    <a href="/" style={{ cursor: 'pointer' }}>
+                        <img src={Logo} alt="Interlex" />
+                    </a>
                 </Box>
-            )}
-        </Box>
+
+                <Box sx={{ width: '35%', maxWidth: '45.5rem' }}>
+                    <Search />
+                </Box>
+
+                {!isLoggedIn ? (
+                    <Box display='flex' gap='1.25rem'>
+                        <Box display='flex' gap='0.25rem'>
+                            <Button>Register</Button>
+                            <Button variant="outlined">Log in</Button>
+                        </Box>
+                        <Divider sx={styles.divider} />
+                        <Button variant="contained">
+                            <AddIcon />
+                            Add a new term
+                        </Button>
+                    </Box>
+                ) : (
+                    <Box display='flex' gap='1.25rem'>
+                        <Button variant="contained">
+                            <AddIcon />
+                            Add a new term
+                        </Button>
+                        <Divider sx={styles.divider} />
+                        <IconButton sx={{
+                            p: 0,
+                            borderRadius: '50%',
+                            '&:focus': {
+                                boxShadow: '0 0 0 0.25rem rgba(152, 162, 179, 0.14)'
+                            }
+                        }}
+                            onClick={handleUserClick} aria-describedby={idUser}
+                        >
+                            <Avatar sx={{ border: '0.0469rem solid rgba(0,0,0,0.08)', width: '2.5rem', height: '2.5rem' }} src="https://mui.com/static/images/avatar/1.jpg" />
+                        </IconButton>
+                        <Popover
+                            sx={{
+                                ...styles.popover,
+                                '& .MuiPopover-paper': {
+                                    padding: '0',
+                                    marginTop: '0.5rem',
+                                },
+
+                                '& .MuiListItem-root + .MuiListItem-root': {
+                                    borderTop: `0.0625rem solid #D3D9D8`,
+                                },
+                                '& .MuiListItem-root': {
+                                    padding: '0.3125rem 0.375rem'
+                                }
+                            }}
+                            id={idUser}
+                            open={openUser}
+                            anchorEl={anchorElUser}
+                            onClose={handleUserClose}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'right',
+                            }}
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                        >
+                            <List>
+                                <ListItem sx={{
+                                    p: '0.75rem 1rem !important',
+
+                                    '& .MuiListItemText-root .MuiListItemText-primary': {
+                                        fontSize: '0.875rem',
+                                        fontWeight: 600,
+                                        lineHeight: '142.857%',
+                                        color: '#3B403F'
+                                    },
+
+                                    '& .MuiListItemText-root .MuiListItemText-secondary': {
+                                        fontSize: '0.875rem',
+                                        fontWeight: 400,
+                                        lineHeight: '142.857%',
+                                        color: '#4D4F4F'
+                                    },
+                                }}>
+                                    <ListItemAvatar>
+                                        <Badge
+                                            variant="dot"
+                                            color="success"
+                                            anchorOrigin={{
+                                                vertical: 'bottom',
+                                                horizontal: 'right',
+                                            }}
+                                        >
+                                            <Avatar sx={{ border: '0.0469rem solid rgba(0,0,0,0.08)', width: '2.5rem', height: '2.5rem' }} src="https://mui.com/static/images/avatar/1.jpg" />
+                                        </Badge>
+                                    </ListItemAvatar>
+                                    <ListItemText
+                                        primary="Olivia Rhye"
+                                        secondary="olivia@untitledui.com"
+                                    />
+                                </ListItem>
+                                {UserNavMenu.map((menu, index) => (
+                                    <ListItem key={index} disablePadding>
+                                        <ListItemButton onClick={(e) => handleMenuClick(e, menu)}>
+                                            <ListItemIcon>
+                                                {menu.icon}
+                                            </ListItemIcon>
+                                            <ListItemText primary={menu.label} />
+                                        </ListItemButton>
+                                    </ListItem>
+                                ))}
+                            </List>
+                        </Popover>
+                    </Box>
+                )}
+            </Box>
+            <EditBulkTermsDialog handleClose={handleCloseEditBulkTerms} open={openEditBulkTerms} activeStep={activeStep} setActiveStep={setActiveStep} />
+        </>
     )
 }
 
