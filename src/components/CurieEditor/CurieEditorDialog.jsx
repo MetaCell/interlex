adf033964317e9d6ababb01298eed9b97669df79
Aiga115 <aiguljhyldyzbekkyzy@gmail.com>
2024-07-22 09:51:22 +0200

    ILEX-30 handle unnecessary api calls and make component more readable

--- src/components/CurieEditor/CurieEditorDialog.jsx
+++ src/components/CurieEditor/CurieEditorDialog.jsx
@@ -2,22 +2,14 @@ import * as React from "react";
 import { Box, Button } from "@mui/material";
 import CustomizedDialog from "../common/CustomizedDialog";
 import { EditNoteIcon } from "../../Icons";
-import BasicTabs from "../common/CustomTabs";
-import CuriesTabPanel from "./CuriesTabPanel";
 import StatusDialog from "../common/StatusDialog";
 
 
-const headCells = [
-    { id: 'prefix', label: 'Prefix' },
-    { id: 'namespace', label: 'Namespace' },
-    { id: 'delete-button', label: '' }
-];
-
-const HeaderRightSideContent = ({ handleClose, handleOpenStatusDialog }) => {
+const HeaderRightSideContent = ({ handleClose, onSaveCuries }) => {
     return (
         <Box display='flex' alignItems='center' gap={1.5}>
             <Button sx={{ p: '0.625rem 0.875rem', minWidth: '0.0625rem' }} variant="outlined" onClick={handleClose}>Cancel</Button>
-            <Button sx={{ p: '0.625rem 0.875rem', minWidth: '0.0625rem' }} variant='contained' onClick={handleOpenStatusDialog}>
+            <Button sx={{ p: '0.625rem 0.875rem', minWidth: '0.0625rem' }} variant='contained' onClick={onSaveCuries}>
                 <EditNoteIcon />
                 Save curies
             </Button>
@@ -25,20 +17,12 @@ const HeaderRightSideContent = ({ handleClose, handleOpenStatusDialog }) => {
     )
 }
 
-const CurieEditorDialog = ({ open, handleClose, image }) => {
-    const [tabValue, setTabValue] = React.useState(0);
+const CurieEditorDialog = ({ open, handleClose, onSubmit, children }) => {
     const [openStatusDialog, setOpenStatusDialog] = React.useState(false);
-    const [myCuries, setMyCuries] = React.useState([]);
-    const [curatedCuries, setCuratedCuries] = React.useState([]);
-    const [latestCuries, setLatestCuries] = React.useState([]);
 
-    const handleChangeTabs = (event, newValue) => {
-        setTabValue(newValue);
-    };
-
-    const handleOpenStatusDialog = () => {
-        console.log("here we connect POST method")
-        setOpenStatusDialog(true)
+    const handleSaveCuries = () => {
+        onSubmit();
+        setOpenStatusDialog(true);
     }
 
     const handleCloseStatusDialog = () => {
@@ -56,24 +40,11 @@ const CurieEditorDialog = ({ open, handleClose, image }) => {
                 HeaderRightSideContent={
                     <HeaderRightSideContent
                         handleClose={handleClose}
-                        handleOpenStatusDialog={handleOpenStatusDialog}
+                        onSaveCuries={handleSaveCuries}
                     />
                 }
             >
-                <Box sx={{ padding: '0.75rem 1.25rem' }}>
-                    <BasicTabs tabValue={tabValue} handleChange={handleChangeTabs} tabs={["My curies", "Curated", "Latest"]} />
-                    <Box flexGrow={1} overflow="auto" p="2.5rem 0.5rem" width={1}>
-                        {
-                            tabValue === 0 && <CuriesTabPanel rows={myCuries} setRows={setMyCuries} curieValue={"base"} editMode headCells={headCells} />
-                        }
-                        {
-                            tabValue === 1 && <CuriesTabPanel rows={curatedCuries} setRows={setCuratedCuries} curieValue={"curated"} editMode headCells={headCells} />
-                        }
-                        {
-                            tabValue === 2 && <CuriesTabPanel rows={latestCuries} setRows={setLatestCuries} curieValue={"latest"} editMode headCells={headCells} />
-                        }
-                    </Box>
-                </Box>
+                {children}
             </CustomizedDialog>
             <StatusDialog
                 title={"Curie editor"}
@@ -83,7 +54,6 @@ const CurieEditorDialog = ({ open, handleClose, image }) => {
                 open={openStatusDialog}
                 handleClose={handleCloseStatusDialog}
                 handleCloseandAdd={handleFinishButtonClick}
-                image={image}
             />
         </>
     )
