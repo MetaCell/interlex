c74d3f2f3529b917a350ac29226e5b97539a2836
vidhya-metacell <vidhya@metacell.us>
2024-08-26 18:34:28 +0200

    Ilex-34 Terms fork UI

--- src/components/SingleTermView/CreateForkDialog.jsx
+++ src/components/SingleTermView/CreateForkDialog.jsx
@@ -1,31 +1,41 @@
-import * as React from "react";
+import { useState } from "react";
 import { vars } from "../../theme/variables";
 import CustomizedDialog from "../common/CustomizedDialog";
-import { Box, Button } from "@mui/material";
+import { Box, Button, Grid, Typography } from "@mui/material";
 import StatusDialog from "../common/StatusDialog";
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import SearchTermsData from "../../static/SearchTermsData.json"
+import CustomSingleSelect from "../common/CustomSingleSelect";
 
-const { gray100, gray200, gray400, brand700 } = vars;
+const { gray800, gray600, gray500, gray700 } = vars;
 
 const HeaderRightSideContent = ({ handleClose, onSaveFork }) => {
   return (
       <Box display='flex' alignItems='center' gap={1.5}>
           <Button sx={{ p: '0.625rem 0.875rem', minWidth: '0.0625rem' }} variant="outlined" onClick={handleClose}>Cancel</Button>
           <Button sx={{ p: '0.625rem 0.875rem', minWidth: '0.0625rem' }} variant='contained' onClick={onSaveFork}>
-              Save curies
+              Create a fork
               <ArrowForwardIcon />
           </Button>
       </Box>
   )
 }
 
-const CreateForkDialog = ({ open, handleClose }) => {
-  const [openStatusDialog, setOpenStatusDialog] = React.useState(false);
-
+const CreateForkDialog = ({ formState, open, handleClose, onInputChange }) => {
+  const [openStatusDialog, setOpenStatusDialog] = useState(false);
+  const [newTerm, setNewTerm] = useState('label')
     const handleSaveFork = () => {
       setOpenStatusDialog(true);
       handleClose()
     }
+    const handleTermChange = (index, field, value) => {
+      // newTerms[index][field] = value;
+      setNewTerm(value)
+    };
+    const updatedColumnsArray = SearchTermsData.termsColumns.map(item => ({
+      ...item,
+      value: item.id
+    }));
 
     const handleCloseStatusDialog = () => {
       setOpenStatusDialog(false)
@@ -37,18 +47,65 @@ const CreateForkDialog = ({ open, handleClose }) => {
     return (
       <>
         <CustomizedDialog
-            title='Create a fork'
-            open={open}
-            handleClose={handleClose}
-            sx={{ '& .MuiDialogContent-root': { padding: 0, overflowY: "hidden" } }}
-            HeaderRightSideContent={
-              <HeaderRightSideContent
-                  handleClose={handleClose}
-                  onSaveFork={handleSaveFork}
-              />
+          title='Create a fork'
+          open={open}
+          handleClose={handleClose}
+          sx={{ '& .MuiDialogContent-root': { overflowY: "hidden" } }}
+          HeaderRightSideContent={
+            <HeaderRightSideContent
+              handleClose={handleClose}
+              onSaveFork={handleSaveFork}
+          />
           }
         >
-            hi
+        <Box width={705}>
+          <Typography color={gray800} fontSize='1.125rem' fontWeight={600} mb='2.75rem'>
+            Add a fork to Central Nervous System
+          </Typography>
+          <Grid container spacing='1.75rem' alignItems={'flex-start'}>
+            <Grid item xs={12} lg={5}>
+              <Box display="flex" justifyContent="space-between">
+                <Typography sx={{
+                  fontSize: '1rem',
+                  fontWeight: '500',
+                  color: gray800,
+                  mb: '.75rem'
+                }}>
+                  Owner
+                </Typography>
+                
+                <Typography variant="body1" sx={{ color: gray600 }}>Required</Typography>
+              </Box>
+              <CustomSingleSelect
+                isFormControlFullWidth={true}
+                options={updatedColumnsArray}
+                placeholder='Select fork owner'
+                value={newTerm}
+                onChange={handleTermChange}
+              />
+            </Grid>
+            <Grid item xs={12} lg={1}>
+              <Box textAlign={'center'} mt="2rem">
+                <Typography variant="body1" fontSize='1.875rem' sx={{ color: gray500 }}>/</Typography>
+              </Box>
+            </Grid>
+            <Grid item xs={12} lg={6}>
+              <Box>
+                <Typography color={gray800} fontSize='1rem' fontWeight={500} mb='1.25rem'>
+                  Fork name
+                </Typography>
+              </Box>
+              <Box>
+                <Typography color={gray700} fontSize='1rem' fontWeight={400}>
+                  Central nervous system
+                </Typography>
+              </Box>
+            </Grid>
+          </Grid>
+          <Typography color={gray600} fontSize='1rem' fontWeight={400} mt='2.75rem'>
+            By default the fork name is the same as the curated. It’s possible to personalise it.
+          </Typography>
+        </Box>
         </CustomizedDialog>
         <StatusDialog
           open={openStatusDialog}
