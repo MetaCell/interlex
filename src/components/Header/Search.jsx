1abd9bc78839eeaafdf2b9a97f64045d286c4dbc
Aiga115 <aiguljhyldyzbekkyzy@gmail.com>
2024-09-02 12:06:08 +0200

    ILEX-68 another merge issues resolving

--- src/components/Header/Search.jsx
+++ src/components/Header/Search.jsx
@@ -1,11 +1,21 @@
-import { Box, Button, Divider, IconButton, TextField, Autocomplete, InputAdornment, Typography, Chip, tabClasses } from "@mui/material";
+import {
+  Box,
+  Button,
+  Divider,
+  IconButton,
+  TextField,
+  Autocomplete,
+  InputAdornment,
+  Typography,
+  Chip,
+  List,
+  ListItem,
+} from "@mui/material";
 import { vars } from "../../theme/variables";
-import { useEffect, useState, useCallback } from 'react';
+import { useEffect, useState, useCallback, forwardRef } from 'react';
 import { searchAll } from "../../api/endpoints";
 import { CloseIcon, ForwardIcon, SearchIcon, TermsIcon } from '../../Icons';
 import FolderOutlinedIcon from '@mui/icons-material/FolderOutlined';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
 import BasicTabs from "../common/CustomTabs";
 import { useNavigate } from "react-router-dom";
 import { debounce } from 'lodash';
@@ -77,37 +87,28 @@ const Search = () => {
   const navigate = useNavigate();
   const query = useQuery();
   const storedSearchTerm = query.get('searchTerm');
-
   const [terms, setTerms] = useState([]);
   const [organizations, setOrganizations] = useState([]);
   const [ontologies, setOntologies] = useState([]);
 
-  const handleOpenList = () => {
-    setOpenList(true);
-  };
+  const handleOpenList = () => setOpenList(true);
+  const handleCloseList = () => setOpenList(false);
+  const handleInputChange = (event) => setSearchTerm(event.target.value);
 
-  const handleCloseList = () => {
-    setOpenList(false);
-  };
-
-  const handleInputChange = (event) => {
-    setSearchTerm(event.target.value);
-  };
-
-  const onSelectTerm = (event, newInputValue) => {
+  const handleSelectTerm = (event, newInputValue) => {
     setSearchTerm("");
     setSelectedValue(newInputValue?.label);
     handleCloseList();
     navigate(`/view?searchTerm=${newInputValue?.label}`);
   };
 
-  const handleClickSearchTerm = () => {
+  const handleSearchTermClick = () => {
     setSelectedValue(searchTerm);
     navigate(`/search?searchTerm=${searchTerm}`);
     handleCloseList();
   };
 
-  const onInputFocus = (event) => {
+  const handleInputFocus = (event) => {
     if (event.target.value) {
       fetchTerms(event.target.value);
     }
@@ -126,10 +127,7 @@ const Search = () => {
 
   useEffect(() => {
     document.addEventListener('keydown', handleKeyDown);
-
-    return () => {
-      document.removeEventListener('keydown', handleKeyDown);
-    };
+    return () => document.removeEventListener('keydown', handleKeyDown);
   }, [handleKeyDown]);
 
   const fetchTerms = useCallback(debounce(async (searchTerm) => {
@@ -154,41 +152,145 @@ const Search = () => {
     }
   }, [storedSearchTerm]);
 
+  const ListboxComponent = forwardRef(function ListboxComponent(props, ref) {
+    return (
+      <>
+        {searchTerm && (<><Box p="0.5rem">
+          <List sx={{
+            '& .MuiTypography-body1': {
+              fontSize: '0.875rem',
+              fontWeight: 500,
+              lineHeight: '142.857%',
+              color: gray800
+            },
+
+            '& .MuiTypography-body2': {
+              fontSize: '0.875rem',
+              flex: 1,
+              fontWeight: 400,
+              lineHeight: '142.857%',
+              color: gray500
+            },
+          }} {...props} onMouseDown={(event) => event.preventDefault()}>
+            <ListItem
+              onClick={handleSearchTermClick}
+              sx={{
+                display: 'flex',
+                gap: '0.5rem',
+                alignItems: 'center',
+                padding: "0.688rem 0.5rem"
+              }}>
+              <SearchIcon />
+              <Typography sx={{ flex: 1 }} variant='body1'>{searchTerm}</Typography>
+              <Button
+                variant='text'
+                sx={{
+                  color: gray700,
+                  p: 0, height: 'auto', lineHeight: 1, background: 'transparent',
+                  '&:hover': {
+                    backgroundColor: 'transparent'
+                  }
+                }}
+              >Browse all</Button>
+            </ListItem>
+          </List>
+        </Box>
+          <Divider sx={{ borderColor: gray200 }} /></>)}
+        <Box>
+          <ListItem sx={{
+            padding: "0.75rem 1rem",
+            "& .MuiTypography-body1": {
+              fontSize: "0.875rem",
+              fontWeight: 500,
+              color: gray600,
+              lineHeight: "1.25rem"
+            },
+            '&:hover': {
+              backgroundColor: 'transparent !important',
+              cursor: 'default'
+            }
+          }}>
+            <Typography variant='body1'>I’m looking for specific type</Typography>
+          </ListItem>
+          <BasicTabs
+            tabValue={tabValue}
+            handleChange={handleChangeTabs}
+            tabs={["Terms", "Organizations", "Ontologies"]}
+            onMouseDown={(event) => event.preventDefault()}
+            parentBoxStyles={{
+              margin: "0 0.875rem"
+            }}
+            tabStyles={{
+              '& .MuiTab-root': {
+                fontSize: "0.875rem",
+                lineHeight: "1.25rem",
+                minHeight: "2rem",
+                padding: "0 0.25rem 0.75rem 0.25rem",
+                minWidth: "3.188rem"
+              }
+            }}
+          />
+          <List sx={{
+            "&.MuiAutocomplete-listbox": {
+              padding: "0.813rem 0.5rem",
+              "& .MuiAutocomplete-option": {
+                padding: "0.5rem"
+              }
+            },
+            "& .MuiTypography-body1": {
+              fontSize: '0.875rem',
+              fontWeight: 500,
+              lineHeight: '142.857%',
+              color: gray800
+            },
+
+            "& .MuiTypography-body2": {
+              fontSize: '0.875rem',
+              flex: 1,
+              fontWeight: 400,
+              lineHeight: '142.857%',
+              color: gray500
+            }
+          }} {...props}>
+            {props?.children}
+          </List>
+        </Box>
+      </>
+    );
+  });
 
   return (
     <Autocomplete
-      sx={{
-        '& .MuiOutlinedInput-root': {
-          borderRadius: openList ? '0.5rem 0.5rem 0 0' : '0.5rem'
-        }
-      }}
+      sx={{ '& .MuiOutlinedInput-root': { borderRadius: openList ? '0.5rem 0.5rem 0 0' : '0.5rem' } }}
       options={tabValue === 0 ? terms : tabValue === 1 ? organizations : ontologies}
-      onChange={onSelectTerm}
-      filterOptions={options => options}
+      onChange={handleSelectTerm}
+      filterOptions={(options) => options}
       open={openList}
       onOpen={handleOpenList}
       onClose={handleCloseList}
       onFocus={handleInputFocus}
       forcePopupIcon={false}
-      renderOption={(props, option, state) => {
-        const { selected } = state;
-
+      getOptionLabel={(option) => option.label || option.name || ''}
+      renderOption={(props, option, { selected }) => {
+        const { key, ...otherProps } = props;
         return (
-          <ListItem sx={{
-            display: "flex",
-            gap: "0.5rem",
-            alignItems: "center",
-            "&:hover": {
-              "& .MuiChip-root": {
-                display: "none",
+          <ListItem
+            key={key}
+            sx={{
+              display: "flex",
+              gap: "0.5rem",
+              alignItems: "center",
+              "&:hover": {
+                "& .MuiChip-root": {
+                  display: "none",
+                },
               },
-            },
-            "&:not(:hover)": {
-              "& .MuiButton-root": {
-                display: "none",
+              "&:not(:hover)": {
+                "& .MuiButton-root": {
+                  display: "none",
+                },
               },
-            },
-          }} {...props}>
+            }} {...otherProps}>
             {tabValue === 0 ? <TermsIcon /> : <FolderOutlinedIcon />}
             <Typography variant="body1">{option?.label || option?.name}</Typography>
             <Typography variant="body2">{option?.submittedBy}</Typography>
@@ -247,113 +349,7 @@ const Search = () => {
           }}
         />
       )}
-      ListboxComponent={(props) => {
-        return (
-          <>
-            {searchTerm && (<><Box p="0.5rem">
-              <List sx={{
-                '& .MuiTypography-body1': {
-                  fontSize: '0.875rem',
-                  fontWeight: 500,
-                  lineHeight: '142.857%',
-                  color: gray800
-                },
-
-                '& .MuiTypography-body2': {
-                  fontSize: '0.875rem',
-                  flex: 1,
-                  fontWeight: 400,
-                  lineHeight: '142.857%',
-                  color: gray500
-                },
-              }} {...props} onMouseDown={(event) => event.preventDefault()}>
-                <ListItem
-                  onClick={handleClickSearchTerm}
-                  sx={{
-                    display: 'flex',
-                    gap: '0.5rem',
-                    alignItems: 'center',
-                    padding: "0.688rem 0.5rem"
-                  }}>
-                  <SearchIcon />
-                  <Typography sx={{ flex: 1 }} variant='body1'>{searchTerm}</Typography>
-                  <Button
-                    variant='text'
-                    sx={{
-                      color: gray700,
-                      p: 0, height: 'auto', lineHeight: 1, background: 'transparent',
-                      '&:hover': {
-                        backgroundColor: 'transparent'
-                      }
-                    }}
-                  >Browse all</Button>
-                </ListItem>
-              </List>
-            </Box>
-              <Divider sx={{ borderColor: gray200 }} /></>)}
-            <Box>
-              <ListItem sx={{
-                padding: "0.75rem 1rem",
-                "& .MuiTypography-body1": {
-                  fontSize: "0.875rem",
-                  fontWeight: 500,
-                  color: gray600,
-                  lineHeight: "1.25rem"
-                },
-                '&:hover': {
-                  backgroundColor: 'transparent !important',
-                  cursor: 'default'
-                }
-              }}>
-                <Typography variant='body1'>I’m looking for specific type</Typography>
-              </ListItem>
-              <BasicTabs
-                tabValue={tabValue}
-                handleChange={handleChangeTabs}
-                tabs={["Terms", "Organizations", "Ontologies"]}
-                onMouseDown={(event) => event.preventDefault()}
-                parentBoxStyles={{
-                  margin: "0 0.875rem"
-                }}
-                tabStyles={{
-                  '& .MuiTab-root': {
-                    fontSize: "0.875rem",
-                    lineHeight: "1.25rem",
-                    minHeight: "2rem",
-                    padding: "0 0.25rem 0.75rem 0.25rem",
-                    minWidth: "3.188rem"
-                  }
-                }}
-              />
-              <List sx={{
-                "&.MuiAutocomplete-listbox": {
-                  padding: "0.813rem 0.5rem",
-                  "& .MuiAutocomplete-option": {
-                    padding: "0.5rem"
-                  }
-                },
-                "& .MuiTypography-body1": {
-                  fontSize: '0.875rem',
-                  fontWeight: 500,
-                  lineHeight: '142.857%',
-                  color: gray800
-                },
-
-                "& .MuiTypography-body2": {
-                  fontSize: '0.875rem',
-                  flex: 1,
-                  fontWeight: 400,
-                  lineHeight: '142.857%',
-                  color: gray500
-                }
-              }} {...props}>
-                {props?.children}
-              </List>
-            </Box>
-          </>
-        )
-      }}
-      inputValue={searchTerm ? searchTerm : selectedValue}
+      ListboxComponent={ListboxComponent}
     />
   );
 };
