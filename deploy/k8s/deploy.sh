7858a9cea0e07fc4c13ac87996d7b299b2674813
jrmartin <jesus@metacell.us>
2025-03-02 12:29:16 -0800

    #100 - Add outbout rules

--- deploy/k8s/deploy.sh
+++ deploy/k8s/deploy.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
-# set to the correct cluster context and namespace
+# Set the correct cluster context and namespace
 kubectl config use-context $CLUSTER_NAME
 kubectl config set-context $CLUSTER_NAME --namespace=$NAMESPACE
 
-# prep the yamls
+# Prep the YAML files
 cp interlex_tpl.yaml interlex.yaml
 cp ingress_tpl.yaml ingress.yaml
+cp allow-outbound_tpl.yaml allow-outbound.yaml  # Add this line
 
-# interlex service and deployment
+# Apply interlex service and deployment
 sed -ie 's/{{TAG}}/'$CF_BUILD_ID'/i' interlex.yaml
 sed -ie 's|{{REGISTRY}}|'$REGISTRY'|i' interlex.yaml
 kubectl apply -f interlex.yaml
 
-# ingress
+# Apply ingress
 sed -ie 's|{{DOMAIN}}|'$DOMAIN'|i' ingress.yaml
 kubectl apply -f ingress.yaml
 
-# cleanup
-rm -rf interlex.yaml* ingress.yaml*
+# Apply network policy
+kubectl apply -f allow-outbound.yaml  # New step to allow outbound traffic
+
+# Cleanup
+rm -rf interlex.yaml* ingress.yaml* allow-outbound.yaml*
