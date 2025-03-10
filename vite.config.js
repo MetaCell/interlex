cd98bd9d2addc073593865fbfe4f093ed084b119
jrmartin <jesus@metacell.us>
2025-03-09 20:50:49 -0700

    #100

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const isProd = process.env.NODE_ENV === "production";
+
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api/elasticsearch": {
-        target: "https://scicrunch.org",
-        changeOrigin: true,
-        secure: true, // Ensure HTTPS requests work
-        rewrite: (path) =>
-          path.replace(/^\/api\/elasticsearch/, "/api/1/elastic/Interlex_pr/_search"),
-        headers: {
-          // Forward headers correctly
-          "Content-Type": "application/json",
+  server: isProd
+    ? {}
+    : {
+        proxy: {
+          "/api/elasticsearch": {
+            target: "https://scicrunch.org",
+            changeOrigin: true,
+            secure: true,
+            rewrite: (path) =>
+              path.replace(/^\/api/elasticsearch/, "/api/1/elastic/Interlex_pr/_search"),
+          },
         },
-      }
-    },
-  },
+      },
 });
