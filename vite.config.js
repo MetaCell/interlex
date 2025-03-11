ce31baa565f71abb6b835eacd66f92035846ad09
jrmartin <jesus@metacell.us>
2025-03-11 10:46:24 -0700

    #100

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const apiKey = process.env.ELASTICSEARCH_API_KEY || import.meta.env.VITE_SCICRUNCH_API_KEY;
+console.log("🛠️ API Key being used:", apiKey);
+
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -9,8 +12,7 @@ export default defineConfig({
         target: "https://scicrunch.org",
         changeOrigin: true,
         secure: true, // Ensure HTTPS requests work
-        rewrite: (path) =>
-          path.replace(/^\/api\/elasticsearch/, "/api/1/elastic/Interlex_pr/_search"),
+        rewrite: (path) => path.replace(/^\/api\/elasticsearch/, `/api/1/elastic/Interlex_pr/_search?key=${apiKey}`),
         headers: {
           // Forward headers correctly
           "Content-Type": "application/json",
