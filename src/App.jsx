93d02184256e8f311cf4aebdc1b0f5b0b796b8ad
Aiga115 <aiguljhyldyzbekkyzy@gmail.com>
2025-04-07 20:19:15 +0200

    ILEX-83 fix linting

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 import CssBaseline from "@mui/material/CssBaseline";
 import { Box, ThemeProvider } from "@mui/material";
 import {
