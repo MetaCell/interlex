39b4bfa3b46cf664ce6aac1b5239a6b33774f03a
ddelpiano <dario@metacell.us>
2025-04-09 10:57:50 +0200

    fix import of base_url

--- src/config.js
+++ src/config.js
@@ -31,5 +31,5 @@ export const API_CONFIG = {
     OLYMPIAN_GODS : "https://uri.olympiangods.org",
     BASE_SCICRUNCH_URL: "/api/elasticsearch?key=",
     SCICRUNCH_KEY: import.meta.env.VITE_SCICRUNCH_API_KEY,
-    BASE_URL: import.meta.env.MODE === "production" ? API_CONFIG.OLYMPIAN_GODS : window.location.origin,
+    BASE_URL: import.meta.env.MODE === "production" ? "https://uri.olympiangods.org" : window.location.origin,
 };
