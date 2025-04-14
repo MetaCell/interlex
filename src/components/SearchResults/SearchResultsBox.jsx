28bd53e409ab89838e372248dfb6f08d2f3469fa
Aiga115 <aiguljhyldyzbekkyzy@gmail.com>
2025-04-14 10:26:20 +0200

    ILEX-104 initial changes make

--- src/components/SearchResults/SearchResultsBox.jsx
+++ src/components/SearchResults/SearchResultsBox.jsx
@@ -29,7 +29,7 @@ const CustomViewButton = ({ view, listView, onClick, icon }) => (
     </Button>
 );
 
-const SearchResultsBox = ({ terms, searchTerm, loading }) => {
+const SearchResultsBox = ({ searchResults, searchTerm, loading }) => {
     const [numberOfVisiblePages, setNumberOfVisiblePages] = React.useState(20);
     const [listView, setListView] = React.useState('list');
 
@@ -41,7 +41,7 @@ const SearchResultsBox = ({ terms, searchTerm, loading }) => {
         <Box width={1} flex={1} display="flex" flexDirection="column" px={4} py={3} gap={3} sx={{ overflowY: 'auto' }}>
             <Grid container justifyContent={{ lg: 'space-between', xs: 'flex-end', md: 'flex-end' }} alignItems="center">
                 <Grid item xs={12} lg={6} sm={6}>
-                    <Typography variant="h5">{terms?.results?.length} results for {searchTerm} search</Typography>
+                    <Typography variant="h5">{searchResults?.results?.length} results for {searchTerm} search</Typography>
                 </Grid>
                 <Grid item xs={12} lg={6} sm={6}>
                     <Box display="flex" alignItems="center" gap={2} justifyContent="end">
@@ -72,9 +72,12 @@ const SearchResultsBox = ({ terms, searchTerm, loading }) => {
                     </Box>
                 </Grid>
             </Grid>
-            {listView === 'list' ? (
-                <ListView searchResults={terms} loading={loading} />
-            ) : (
+            {listView === 'list' ? (<>
+                {Object.entries(searchResults).map(([key, value]) => (
+                    value.length > 0 && (
+                    <ListView key={key} searchResults={value} loading={loading} />
+                )))}
+            </>) : (
                 <p>table</p>
             )}
         </Box>
@@ -89,7 +92,7 @@ CustomViewButton.propTypes = {
 };
 
 SearchResultsBox.propTypes = {
-    terms: PropTypes.object,
+    searchResults: PropTypes.object,
     searchTerm: PropTypes.string,
     loading: PropTypes.bool
 };
--- src/components/SearchResults/index.jsx
+++ src/components/SearchResults/index.jsx
@@ -1,21 +1,23 @@
 import { debounce } from 'lodash';
 import PropTypes from 'prop-types';
 import { useQuery } from '../../helpers';
-import termParser from '../../parsers/termParser';
 import SearchResultsBox from './SearchResultsBox';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import FiltersSidebar from '../Sidebar/FiltersSidebar';
-import * as mockApi from '../../api/endpoints/swaggerMockMissingEndpoints';
+import { searchAll, elasticSearch } from '../../api/endpoints';
+import { SEARCH_TYPES } from '../../constants/types';
 
-const useMockApi = () => mockApi;
 
 const SearchResults = () => {
-    const [loading, setLoading] = useState(false);
-    const [terms, setTerms] = useState({});
+    const [loading, setLoading] = useState(true);
     const [filters, setFilters] = useState([]);
     const [checkedLabels, setCheckedLabels] = useState({});
+    const [searchResults, setSearchResults] = useState({
+        terms: [],
+        organizations: [],
+        ontologies: []
+    });
     const query = useQuery();
-    const { getMatchTerms } = useMockApi();
 
     const searchTerm = query.get('searchTerm');
 
@@ -27,38 +29,56 @@ const SearchResults = () => {
                 [label]: !prev[category]?.[label]
             }
         }));
+        filteredResults()
     };
 
-    const fetchTerms = useRef(
-        debounce((searchTerm) => {
-            setLoading(true);
-            getMatchTerms("i")
-                .then((data) => {
-                    const parsedData = termParser(data, searchTerm);
-                    setTerms(parsedData);
-                    setFilters(parsedData.filters);
-                    setLoading(false);
-                })
-                .catch((error) => {
-                    setLoading(false);
-                    console.error(error);
-                });
-        }, 500)
-    ).current;
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    const fetchTerms = useCallback(debounce(async (searchTerm) => {
+        const data = await elasticSearch(searchTerm);
+        setFilters(data.filters)
+        setSearchResults({
+            terms: data?.results?.filter(result => result.type === SEARCH_TYPES.TERM) || [],
+            organizations: data?.results?.filter(result => result.type === SEARCH_TYPES.ORGANIZATION) || [],
+            ontologies: data?.results?.filter(result => result.type === SEARCH_TYPES.ONTOLOGY) || []
+        });
+        setLoading(false)
+    }, 500), [searchAll]);
 
     useEffect(() => {
         fetchTerms(searchTerm);
     }, [searchTerm, fetchTerms]);
 
 
-    const filterResults = (results, checkedLabels) => {
-        return results.filter(item => {
-            for (let category in checkedLabels) {
-                if (!checkedLabels[category]) continue; // Skip empty categories
-                for (let label in checkedLabels[category]) {
-                    if (checkedLabels[category][label]) {
-                        const categoryLower = category.toLowerCase();
-                        const itemValue = item[categoryLower] || item[categoryLower === 'type' ? 'Type' : categoryLower]; // Case-insensitive check
+    // const filterResults = (results, checkedLabels) => {
+    //     return results.filter(item => {
+    //         for (let category in checkedLabels) {
+    //             if (!checkedLabels[category]) continue; // Skip empty categories
+    //             for (let label in checkedLabels[category]) {
+    //                 if (checkedLabels[category][label]) {
+    //                     const categoryLower = category.toLowerCase();
+    //                     const itemValue = item[categoryLower] || item[categoryLower === 'type' ? 'Type' : categoryLower]; // Case-insensitive check
+    //                     if (itemValue !== label) {
+    //                         return false;
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //         return true;
+    //     });
+    // };
+    const filterResults = (results, checkedLabels, category) => {
+        const arrayToFilter = results[category] || [];
+        
+        return arrayToFilter.filter(item => {
+            for (let filterCategory in checkedLabels) {
+                if (!checkedLabels[filterCategory]) continue; // Skip empty categories
+                
+                for (let label in checkedLabels[filterCategory]) {
+                    if (checkedLabels[filterCategory][label]) {
+                        const categoryLower = filterCategory.toLowerCase();
+                        const itemValue = item[categoryLower] || 
+                                         item[categoryLower === 'type' ? 'Type' : categoryLower]; // Case-insensitive check
+                        
                         if (itemValue !== label) {
                             return false;
                         }
@@ -69,12 +89,21 @@ const SearchResults = () => {
         });
     };
 
-    const filteredResults = filterResults(terms?.results || [], checkedLabels);
+    // const filteredResults = filterResults(searchResults || [], checkedLabels);
+    const filteredResults = () => {
+        setSearchResults({
+            terms: filterResults(searchResults, checkedLabels, 'terms'),
+            organizations: filterResults(searchResults, checkedLabels, 'organizations'),
+            ontologies: filterResults(searchResults, checkedLabels, 'ontologies')
+        });
+    };
+
+    console.log("searchResults: ", searchResults)
 
     return (
         <>
             <FiltersSidebar filters={filters} checkedLabels={checkedLabels} handleCheckboxChange={handleCheckboxChange} />
-            <SearchResultsBox terms={filteredResults} searchTerm={searchTerm} loading={loading} />
+            <SearchResultsBox searchResults={searchResults} searchTerm={searchTerm} loading={loading} />
         </>
     );
 };
